1cb607a2ee8cc60c333e88ba04817f9830f6d77c
Faith <faithdeffor@gmail.com>
2026-05-07 19:45:41 -0400

    updated messages to send every 15 minutes

--- src/bluetooth_notifications.cpp
+++ src/bluetooth_notifications.cpp
@@ -3,23 +3,38 @@
 
 BluetoothSerial SerialBT;
 
-const unsigned long STATUS_INTERVAL = 30UL * 60UL * 1000UL;
+const unsigned long STATUS_INTERVAL = 15UL * 60UL * 1000UL;
 unsigned long lastStatusTime = 0;
+bool wasConnected = false;
 
 void beginBluetooth() {
     SerialBT.begin("PlantMonitor");
 }
 
 void sendRedZoneAlert() {
-    SerialBT.println("ALERT: Plant needs water urgently!");
+    if (SerialBT.hasClient()) {
+        SerialBT.println("ALERT: Plant needs water urgently!");
+    }
 }
 
 void sendStatusUpdate(int moisturePercent, unsigned long currentTime) {
-    if (currentTime - lastStatusTime >= STATUS_INTERVAL) {
+    bool isConnected = SerialBT.hasClient();
+
+    if (isConnected && !wasConnected) {
+        SerialBT.println("Connected to PlantMonitor.");
+        SerialBT.print("Current moisture level: ");
+        SerialBT.print(moisturePercent);
+        SerialBT.println("%");
+
+        lastStatusTime = currentTime;
+    }
+    
+    if (isConnected && currentTime - lastStatusTime >= STATUS_INTERVAL) {
         lastStatusTime = currentTime;
 
         SerialBT.print("Status update: Moisture is ");
         SerialBT.print(moisturePercent);
         SerialBT.println("%");
     }
+    wasConnected = isConnected;
 }
